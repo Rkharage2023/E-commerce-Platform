53278620c9ef8b7670d226966dcdbb271cf685d4
Rushikesh Kharage <rushikeshkharage04@gmail.com>
2026-03-16 21:53:24 +0530

    server.js change

--- e-commerce-app/server.js
+++ e-commerce-app/server.js
@@ -14,13 +14,13 @@ import orderRoutes from "./src/routes/orderRoutes.js";
 import adminEmployeeRoutes from "./src/routes/adminEmployeeRoutes.js";
 
 import "./src/config/passport.js";
-
 // ==========================
 // CONFIG
 // ==========================
 dotenv.config();
 
 const app = express();
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 5000;
 
 // ==========================
@@ -94,6 +94,17 @@ app.use((err, req, res, next) => {
 // ==========================
 // START SERVER
 // ==========================
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Server failed to start:", error);
+  }
+};
+
+startServer();
